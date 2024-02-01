1a3333a6e2fc4fe8ece150f316c4c2bc0f4d8f28
ZXSIROTIR <92199008+ZXSIROTIR@users.noreply.github.com>
2024-02-01 22:58:28 +0700

    2
    Сделал открытие файла, и запись из файла в двумерный массив и функцию вывода в консольку

--- lab_gaus/main.cpp
+++ lab_gaus/main.cpp
@@ -1,14 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <limits>
 
-void LoadFile(){
-	std::ifstream inputFile("SLAU.txt", std::ios::binary);
-	if (!inputFile) {
-    	std::cout << "Error opening file!" << std::endl;
-        return 1;
+using namespace std;
+
+void printMatrix(int** matrix, int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size + 1; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
-int main (){
-	
+void gauss(int** matrix, int size){
 }
+
+
+int main() {
+    ifstream inputFile("SLAU.txt");
+    if (!inputFile.is_open()) {
+        cerr << "eror opening file" << endl;
+        return 1;
+    }
+
+    int size;
+    inputFile >> size;
+
+    int** matrix = new int*[size];
+    for (int i = 0; i < size; ++i) {
+        matrix[i] = new int[size + 1];
+    }
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size + 1; ++j) {
+            if (j == size) {
+                char delimiter;
+                inputFile >> delimiter;
+            }
+            inputFile >> matrix[i][j];
+        }
+    }
+    inputFile.close();
+
+    cout << "matrix:" << endl;
+    printMatrix(matrix, size);
+
+    return 0;
+}
+
